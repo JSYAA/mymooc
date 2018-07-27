3787e6c997b78f1adbaf1cc2d56c961c84c3556c
JSY <908575378@qq.com>
2018-07-27 15:58:36 +0800

    users-model

--- users/models.py
+++ users/models.py
@@ -1,4 +1,5 @@
 # _*_ encoding:utf-8 _*_
+
 from __future__ import unicode_literals
 from datetime import datetime
 
@@ -32,4 +33,16 @@ class EmailVerifyRecord(models.Model):
 
     class Meta:
         verbose_name = u"邮箱验证码"
+        verbose_name_plural = verbose_name
+
+
+class Banner(models.Model):
+    title = models.CharField(max_length=100, verbose_name=u"标题")
+    image = models.ImageField(upload_to="banner/%Y/%m", verbose_name=u"轮播图")
+    url = models.URLField(max_length=200, verbose_name=u"访问地址")
+    index = models.IntegerField(default=100, verbose_name=u"顺序")
+    add_time = models.DateTimeField(default=datetime.now, verbose_name=u"添加时间")
+
+    class Meta:
+        verbose_name = u"轮播图"
         verbose_name_plural = verbose_name
